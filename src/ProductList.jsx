44de545096dfa2b8229743f172e35cf84656e753
RickyLes <leszczynski.ricky@gmail.com>
2026-09-08 15:02:13 -0400

    Added styled response to cart buttons

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -328,10 +328,11 @@ function ProductList({ onHomeClick }) {
                                         <div className="product-description">{plant.description}</div>
                                         <div className="product-cost">${plant.cost}</div>
                                         <button
-                                            className="product-button"
+                                            className={`product-button ${addedToCart[plant.name] ? 'added-to-cart' : ''}`}
                                             onClick={() => handleAddToCart(plant)}
+                                            disbled={addedToCart[plantName]}
                                         >
-                                            Add to Cart
+                                            {addedToCart[plant.name] ? 'Added to Cart' : 'Add to Cart'}
                                         </button>
                                     </div>
                                 ))}
