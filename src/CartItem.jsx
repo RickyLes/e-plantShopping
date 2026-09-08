5287322aba2052c55cf82fcaa2ad7b574616394e
RickyLes <leszczynski.ricky@gmail.com>
2026-09-08 14:15:27 -0400

    Added alert that checkout is not functional yet.

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -33,6 +33,10 @@ const CartItem = ({ onContinueShopping }) => {
     dispatch(removeItem(item.name));
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference.')
+  }
+
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
     const price = Number.parseFloat(item.cost.replace('$', ''));
@@ -64,7 +68,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
