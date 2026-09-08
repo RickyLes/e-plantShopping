ae37264452df111f10e54aeae10ed5451b256cc1
RickyLes <leszczynski.ricky@gmail.com>
2026-09-08 14:00:44 -0400

    Added cart functions

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -4,32 +4,39 @@ import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
-  const cart = useSelector(state => state.cart.items);
-  const dispatch = useDispatch();
+    const cart = useSelector(state => state.cart.items);
+    const dispatch = useDispatch();
 
-  // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
-  };
+    // Calculate total amount for all products in the cart
+    const calculateTotalAmount = () => {
+        return cart.reduce((total, item) => total + Number(calculateTotalCost(item)), 0).toFixed(2);
+    };
 
-  const handleContinueShopping = (e) => {
-   
+    const handleContinueShopping = (e) => {
+        e.preventDefault();
+        onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity >1 ) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity -1 }));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const price = Number.parseFloat(item.cost.replace('$', ''));
+    return (price * item.quantity).toFixed(2);
   };
 
   return (
